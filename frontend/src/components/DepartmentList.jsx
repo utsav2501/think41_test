b6cd2a618e9d9407b2d6ba2ad34b5569521cb430
utsav2501 <utsav2112088@akgec.ac.in>
2025-07-31 15:12:35 +0530

    some change

--- frontend/src/components/DepartmentList.jsx
+++ frontend/src/components/DepartmentList.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function DepartmentList() {
     // This component will fetch and display a list of departments
--- frontend/src/components/DepartmentPage.jsx
+++ frontend/src/components/DepartmentPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios';
 
 function DepartmentPage() {
     const {id} = useParams();
